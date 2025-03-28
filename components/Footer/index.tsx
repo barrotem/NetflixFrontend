c1cc1a9dfa7a7cfad2043edb03373f0daff848e1
Bar Rotem <76170839+barrotem@users.noreply.github.com>
2025-03-28 09:59:04 +0300

    Change Author name Feature (#3)
    * Return of ayushi
    
    * another change for author

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <p>
         Made by{' '}
         <a href='https://github.com/ayushiee/nextflix' target='_blank' rel='noreferrer'>
-          <b>Ayushi Gupta</b>
+          <b>Ayushi Gupta comes back - Oh no i forgot something !</b>
         </a>
       </p>
     </div>
